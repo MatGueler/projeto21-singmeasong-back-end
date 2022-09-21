5457dbfabb57dc87e27d764f93f4f8971b0a5852
MattGueler <mateus.gueler@gmail.com>
2022-09-21 16:22:50 -0300

    Create a random test id

--- tests/factory/createRecomendationsFactory.ts
+++ tests/factory/createRecomendationsFactory.ts
@@ -1,3 +1,5 @@
+import { prisma } from "../../src/database";
+
 async function createRandomRecomendation() {
   return {
     name: "Falamansa - Xote dos Milagres",
@@ -12,7 +14,56 @@ async function createBadRecomendation() {
   };
 }
 
+async function createRandomicId() {
+  const number = Math.random();
+
+  if (number <= 0.7) {
+    let id = await scoreGreaterTen();
+    if (id) {
+      id = await scoreLessTen();
+      if (id) {
+        return id;
+      }
+    }
+  } else {
+    let id = await scoreLessTen();
+    if (id) {
+      id = await scoreGreaterTen();
+      if (id) {
+        return id;
+      }
+    }
+  }
+  const id = await getAllRecommendations();
+  return id;
+}
+
+async function getAllRecommendations() {
+  const recommendations = await prisma.recommendation.findMany();
+  const randomId = Math.floor(Math.random() * (recommendations.length - 1));
+  const randomRecomendation = recommendations[randomId];
+  return randomRecomendation;
+}
+
+async function scoreLessTen() {
+  const recommendations = await prisma.recommendation.findMany({
+    where: { score: { lte: 10 } },
+  });
+  const randomId = Math.floor(Math.random() * (recommendations.length - 1));
+  const randomRecomendation = recommendations[randomId];
+  return randomRecomendation;
+}
+async function scoreGreaterTen() {
+  const recommendations = await prisma.recommendation.findMany({
+    where: { score: { gt: 10 } },
+  });
+  const randomId = Math.floor(Math.random() * (recommendations.length - 1));
+  const randomRecomendation = recommendations[randomId];
+  return randomRecomendation;
+}
+
 export const generateFactory = {
   createRandomRecomendation,
   createBadRecomendation,
+  createRandomicId,
 };
--- tests/name.test.ts
+++ tests/name.test.ts
@@ -7,14 +7,14 @@ import { getDataFactory } from "./factory/getDataRecomendation";
 
 dotenv.config();
 
-beforeEach(async () => {
-  await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
-});
+// beforeEach(async () => {
+//   await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
+// });
 
-afterAll(async () => {
-  await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
-  prisma.$disconnect();
-});
+// afterAll(async () => {
+//   await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
+//   prisma.$disconnect();
+// });
 
 const agent = supertest(app);
 
@@ -57,5 +57,30 @@ describe("POST /recommendations/:id/downvote", () => {
     await agent.post(`/recommendations/${id}/downvote`).send();
     const result = await getDataFactory.getScoreRecomendation();
     expect(result).toBe(0);
+  });
+});
+
+describe("GET /recommendations", () => {
+  it("Get 10 recomendations", async () => {
+    const result = await agent.get(`/recommendations`).send();
+    expect(result.status).toBe(200);
+  });
+});
+
+describe("GET /recommendations/:id", () => {
+  it("Get recomendations by id", async () => {
+    const id = await getDataFactory.getIdRecomendation();
+    const result = await agent.get(`/recommendations/${id}`).send();
+    expect(result.status).toBe(200);
+  });
+});
+
+describe("GET /recommendations/random", () => {
+  it("Get recomendations by randomic id", async () => {
+    await generateFactory.createRandomRecomendation();
+    const id = await generateFactory.createRandomicId();
+    console.log(id);
+    // const result = await agent.get(`/recommendations/${id}`).send();
+    // expect(result.status).toBe(200);
   });
 });
